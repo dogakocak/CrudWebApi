4b709ab86db9ad7f5cfd567aeb08fb151bc4a3b2
dogakocak <dogakocak>
2023-07-24 16:58:08 +0300

    Save method implemented, lazy loading

---
+++ CrudWeb.Repositories/Contexts/RepositoryManager.cs
@@ -0,0 +1,20 @@
+﻿using Repositories.Contracts;
+
+namespace Repositories.Contexts;
+
+public class RepositoryManager : IRepositoryManager
+{
+
+    private readonly CrudDbContext _crudDbContext;
+    private readonly Lazy<IProductRepository> _productRepository; // Lazy loading
+
+    public RepositoryManager(CrudDbContext crudDbContext)
+    {
+        _crudDbContext = crudDbContext;
+        _productRepository = new Lazy<IProductRepository>(() => new ProductRepository(crudDbContext));
+    }
+
+    public IProductRepository ProductRepository => _productRepository.Value;
+
+    public void Save() => _crudDbContext.SaveChanges();
+}
