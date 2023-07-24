f8a93264197178d2ece75b4d89db23cfedb97d89
dogakocak <dogakocak>
2023-07-24 14:55:57 +0300

    Product repository is implemented

---
+++ CrudWeb.Repositories/Contexts/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using CrudWeb.Models.Products;
+using Repositories.Contracts;
+
+namespace Repositories.Contexts;
+
+public class ProductRepository : RepositoryBase<Product>, IProductRepository
+{
+    public ProductRepository(CrudDbContext crudDbContext) : base(crudDbContext)
+    {
+    }
+
+    public IQueryable<Product> GetAllProducts(bool trackChanges) => FindAll(trackChanges)
+        .OrderBy(p => p.Name);
+
+    public IQueryable<Product> GetProductById(Guid id, bool trackChanges) =>
+        FindByConditions(p => p.Id == id,trackChanges);
+
+    public void CreateOneProduct(Product product) => Create(product);
+
+    public void UpdateOneProduct(Product product) => Update(product);
+
+    public void DeleteOneProduct(Product product) => Delete(product);
+}
---
+++ CrudWeb.Repositories/Contracts/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using CrudWeb.Models.Products;
+
+namespace Repositories.Contracts;
+
+public interface IProductRepository : IRepositoryBase<Product>
+{
+
+    IQueryable<Product> GetAllProducts(bool trackChanges);
+    IQueryable<Product> GetProductById(Guid id, bool trackChanges);
+    void CreateOneProduct(Product product);
+    void UpdateOneProduct(Product product);
+    void DeleteOneProduct(Product product);
+}
