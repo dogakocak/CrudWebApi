af0d7230037fa8cf39c15580325d8cd47e20f7f9
dogakocak <dogakocak>
2023-07-24 14:31:57 +0300

    RepositoryBase.cs is created

--- CrudWeb.Repositories/Contexts/RepositoryBase.cs
+++ CrudWeb.Repositories/Contexts/RepositoryBase.cs
@@ -3,10 +3,15 @@ using Repositories.Contracts;
 
 namespace Repositories.Contexts;
 
-public class RepositoryBase<T> : IRepositoryBase<T>
+public abstract class RepositoryBase<T> : IRepositoryBase<T>
 where T: class
 {
-    
+    protected readonly CrudDbContext CrudDbContext;
+    public RepositoryBase(CrudDbContext crudDbContext)
+    {
+        CrudDbContext = crudDbContext;
+    }
+
     public IQueryable<T> FindAll(bool trackChanges)
     {
         throw new NotImplementedException();
@@ -17,18 +22,9 @@ where T: class
         throw new NotImplementedException();
     }
 
-    public void Create(T entity)
-    {
-        throw new NotImplementedException();
-    }
+    public void Create(T entity) => CrudDbContext.Set<T>().Add(entity);
 
-    public void Update(T entity)
-    {
-        throw new NotImplementedException();
-    }
+    public void Update(T entity) => CrudDbContext.Set<T>().Update(entity);
 
-    public void Delete(T entity)
-    {
-        throw new NotImplementedException();
-    }
+    public void Delete(T entity) => CrudDbContext.Set<T>().Remove(entity);
 }
