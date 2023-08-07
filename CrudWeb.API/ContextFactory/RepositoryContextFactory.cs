b2f6782ab827f7f53888f8f7eb54715daf775cba
dogakocak <dogakocak>
2023-08-07 23:01:31 +0300

    RepositoryContextFactory.cs created

---
+++ CrudWeb.API/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repositories.Contexts;
+
+namespace WebApplication3.ContextFactory;
+
+public class RepositoryContextFactory : IDesignTimeDbContextFactory<CrudDbContext>
+{
+    public CrudDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<CrudDbContext>()
+            .UseMySQL(configuration.GetConnectionString("DefaultConnection"),
+                prj => prj.MigrationsAssembly("CrudWeb.API")
+                );
+
+        return new CrudDbContext(builder.Options);
+    }
+}
